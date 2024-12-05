cf844b05125b79c69bfc87609f02045a1b5291d5
RAHILxARYAN <rahilxaryan@outlook.com>
2024-12-05 11:41:06 +0530

    first commit

---
+++ code.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo hello world  
+
+name=rahil
+age=20
+echo "my name is $name and my age is $age"
+
+current_path=$(pwd)
+
+host=$(hostname)
+
+echo "i am in $pwd and my host is $hostname"
